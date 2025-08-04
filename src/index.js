70572ba381d390c8a4662013bc888ec62a8f093f
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2025-08-04 17:55:59 -0400

    Site Basic Functionality Complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "./style.css";
-console.log("Webpack template!");
+
+let form = document.querySelector("form")
+let input = document.querySelector("#MainInput")
+let toggletempbutton = document.querySelector("#toggletemp")
+function toggletemp(){
+    if(toggletempbutton.getAttribute("data-temptype") == "C"){
+        toggletempbutton.setAttribute("data-temptype","F")
+        toggletempbutton.textContent = "F°"
+    } else {
+        toggletempbutton.setAttribute("data-temptype","C")
+        toggletempbutton.textContent = "C°"
+    }
+}
+toggletempbutton.addEventListener("click", toggletemp)
+form.addEventListener('submit',(event) => {
+    event.preventDefault()
+    Main(input.value)
+} );
+
 async function GetWeatherData(location){
     try{
         let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=EVNU8SLUCWZCQKN2YKU82VNWL&contentType=json`)
@@ -21,7 +39,7 @@ function ProcessData(data){
         alert: data.alerts,
         feelslike: data.currentConditions.feelslike,
         temperature: data.currentConditions.temp,
-        precipitationprobability: data.currentConditions.precipitationprobability,
+        precipitationprobability: data.currentConditions.precipprob,
         description: data.description,
         icon: data.currentConditions.icon
     }
@@ -29,18 +47,64 @@ function ProcessData(data){
     console.log(relevantData)
     return relevantData
 }
-function UpdateDom(){
-
+async function GetIcon(iconName) {
+    try {
+        const icon = await import(`./images/WeatherSvg/${iconName}.svg`);
+        return icon.default;
+    } catch {
+        console.error(`Missing icon for ${iconName}`);
+        return null;
+    }
 }
+const DomController = (function(){
+    let condition = document.querySelector("#condition")
+    let resolvedaddress = document.querySelector("#resolvedaddress")
+    let weathersymbol = document.querySelector("#weathersymbol")
+    let temperature = document.querySelector("#temperature")
+    let precipitationprobability = document.querySelector("#precprob")
+    let description = document.querySelector("#description")
+    let alert = document.querySelector("#alert")
+    let maingrid = document.querySelector('.grid')
+    async function UpdateDom(data){
+        HideWeather()
+        condition.textContent = data.condition
+        resolvedaddress.textContent = data.resolvedAddress
+        precipitationprobability.textContent = data.precipitationprobability + "% Chance of rain"
+        description.textContent = data.description
+        let iconname = data.icon
+        let iconsrc = await GetIcon(iconname)
+        weathersymbol.setAttribute("src",iconsrc)
+        if(data.alert.length > 0){
+            let alertobject = data.alert[0]
+            alert.textContent = alertobject.description
+        } else{
+            alert.textContent = "No Active Alerts"
+        }
+        if (toggletempbutton.getAttribute("data-temptype") == "C"){
+            temperature.textContent = `Temperature: ${data.temperature}C° Feels Like: ${data.feelslike}C°`
+        } else {
+            temperature.textContent = `Temperature: ${(data.temperature*9/5) + 32}F° Feels Like: ${(data.feelslike*9/5) + 32}F°`
+        }
+       
+        ShowWeather()
+    }
+    function HideWeather (){
+        maingrid.style.display = 'none'
+    }
+    function ShowWeather(){
+        maingrid.style.display = 'grid'
+    }
+    return{UpdateDom,HideWeather,ShowWeather}
+})()
 async function Main(location){
     let weatherdata = await GetWeatherData(location)
 
     if(weatherdata){
-        ProcessData(weatherdata)
+        let proccesseddata = ProcessData(weatherdata)
+        DomController.UpdateDom(proccesseddata)
     }
 
+    
     // Will eventually call updatedom and other functions here after i have made them. Everything is in one js file as this is a small project with low scope
 }
 
-Main('Toronto')
-Main('Tokyo')
